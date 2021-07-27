2625c196787abfe07970d16acd77fc4850ceca34
RaviNikkam <33675647+RaviNikkam@users.noreply.github.com>
2021-07-27 14:41:39 +0530

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://a513ffd1.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
